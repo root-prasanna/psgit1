8080bf9f62ce59067e9ce53229f6fc68fb6c8f38
root-prasanna <prasannashah.04@gmail.com>
2017-05-05 16:27:06 +0545

    first commit

---
+++ email.py
@@ -0,0 +1,13 @@
+import re
+email = raw_input("Please type in an email address: ")
+if re.match("\A(?P<name>[\w\-_]+)@(?P<domain>[\w\-_]+).(?P<toplevel>[\w]+)\Z",email,re.IGNORECASE):
+    	print("Email is valid")
+else:
+    	print("Email is invalid")
+
+
+
+
+
+
+
